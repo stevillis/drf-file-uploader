9fbb9145a51e79a5154bcddab702df7b2c0d26f8
Stévillis Sousa <stevillis@hotmail.com>
2022-06-06 23:18:23 -0400

    document version model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class DocumentVersion(models.Model):
+    name = models.CharField(max_length=255)
+    doc_file = models.FileField(upload_to='media/')
